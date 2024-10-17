5cf9f42b6a7f22e41aef0988c2c1618a68940312
VJ-trend <vijaytrendnologies@gmail.com>
2024-10-17 19:13:43 +0530

    first commit

---
+++ src/test/java/com/facebook2/runner/Runnerclass.java
@@ -0,0 +1,39 @@
+package com.facebook2.runner;
+
+import org.junit.runner.RunWith;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import com.github.dockerjava.api.model.Driver;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+@RunWith(Cucumber.class)
+
+@CucumberOptions(features = "C:\\Users\\vijay\\eclipse-workspace\\Maven-cucumber\\Facebook1\\src\\test\\java\\com\\Facebook\\feature\\Facebook.feature",
+glue = "com.facebook2.stepdefinition")
+
+public class Runnerclass {
+
+	// Hybrid framework
+	// annotation - junit [framework] - small framework
+
+	static WebDriver driver;
+
+	public static void browserlaunch() {
+
+		WebDriverManager.chromedriver().setup();
+
+		driver = new ChromeDriver();
+
+		driver.get("https://www.facebook.com/index.php/");
+
+	}
+
+	public static void close() {
+		driver.close();
+	}
+
+}
